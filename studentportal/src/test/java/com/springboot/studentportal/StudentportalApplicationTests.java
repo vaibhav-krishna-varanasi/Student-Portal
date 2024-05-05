9578b3c66f32e42a7a624170a569ae6d5a6f2759
vaibhav-krishna-varanasi <varanasivaibhavkrishna@gmail.com>
2024-05-05 14:54:44 -0500

    initial commit.

---
+++ studentportal/src/test/java/com/springboot/studentportal/StudentportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studentportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
